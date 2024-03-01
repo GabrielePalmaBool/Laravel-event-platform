5eccc6be8f1305e5d8aa1e8bcbdbe7e348f2c06e
LeandroL18 <leandrolipani6@gmail.com>
2024-03-01 15:40:53 +0100

    feat: set front-end

---
+++ front-end/vitejs-api/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import EventList from './components/EventList.vue'
+</script>
+
+<template>
+  <EventList/>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+
+</style>
---
+++ front-end/vitejs-api/src/components/EventList.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios'
+
+export default{
+  name:'EventList',
+  data(){
+    return{
+      events: [],
+      createActive: false,
+      newEvent:{
+        name:''
+      }
+    }
+  }
+}
+
+</script>
+
+<template>
+ <h1>Hello</h1>
+</template>
+
+<style scoped lang="scss">
+@use "./styles/general.scss";
+
+</style>
